cbde508033082f0f3908a10bcb07e2c139f3fb6e
davidlutch <david.lutch@menloschool.org>
2025-09-20 23:35:14 -0700

    Finished! Just need to add comments later

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -19,18 +19,7 @@ public class HighwaysAndHospitals {
             // Builds a hospital in every city because it's cheaper than building highways
             return ((long) n * hospitalCost);
         }
-        int minimalCost = 0;
-        // figure out how many subgraphs
-        //int[] map = new int[]
-// For each edge AB:
-//	While city A is not its root:
-//		A = roots[A]
-//	While city B is not its root:
-//		B = roots[B]
-//	If A != B:
-//		roots[B] = A
-//
-//Calculate cost and return
+        long minimalCost = 0;
         // Left and right city
         int cityOne;
         int cityTwo;
@@ -42,34 +31,49 @@ public class HighwaysAndHospitals {
         for (int i = 0; i < cities.length; i++) {
             cityOne = cities[i][0];
             rootOne = cityOne;
-            while (map[rootOne] != 0) {
+            while (map[rootOne] > 0) {
                 rootOne = map[rootOne];
             }
             cityTwo = cities[i][1];
             rootTwo = cityTwo;
-            while (map[rootTwo] != 0) {
+            while (map[rootTwo] > 0) {
                 rootTwo = map[rootTwo];
             }
             // Path compression (53-62)
-            while (map[cityOne] != 0) {
+            while (map[cityOne] > 0) {
                 tracker = map[cityOne];
                 map[cityOne] = rootOne;
                 cityOne = tracker;
             }
-            while (map[cityTwo] != 0) {
+            while (map[cityTwo] > 0) {
                 tracker = map[cityTwo];
                 map[cityTwo] = rootTwo;
                 cityTwo = tracker;
             }
             // Weight balancing
+            if (rootOne != rootTwo) {
+                // Checks to see how many cities are connected to the first root
+                if (map[rootOne] > map[rootTwo]) {
+                    // Minusing one from the total cities connected to root two because I want to include
+                    // the root itself
+                    map[rootTwo] += map[rootOne] - 1;
+                    map[rootOne] = rootTwo;
+                }
+                else {
+                    // Minusing one from the total cities connected to root two because I want to include
+                    // the root itself
+                    map[rootOne] += map[rootTwo] - 1;
+                    map[rootTwo] = rootOne;
+                }
+            }
         }
         int counter = 0;
-        for (int i = 0; i < map.length; i++) {
-            if (map[i] == 0) {
+        for (int i = 1; i < map.length; i++) {
+            if (map[i] <= 0) {
                 counter++;
             }
         }
-        minimalCost = (counter * hospitalCost) + ((n-counter) * highwayCost);
+        minimalCost = ((long) counter * hospitalCost) + ((long) (n - counter) * highwayCost);
         return minimalCost;
     }
 
